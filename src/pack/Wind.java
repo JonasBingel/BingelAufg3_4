2b4bace7dde36913d6fd8d8fc369d42f4707dd47
Jonas Bingel <30416267+JonasBingel@users.noreply.github.com>
2018-11-27 20:25:59 +0100

    Update Wind.java

--- src/pack/Wind.java
+++ src/pack/Wind.java
@@ -1,8 +1,18 @@
 package pack;
+// TODO eventuell toString Methode mit if (momentanerWind.equals(Windart.KEIN_WIND)) return "" else return "Wind!";
 
+/**
+ * @author Jonas Bingel Wind-Klasse fuer die Volocopter Klasse, in der Wind
+ *         simuliert werden kann.
+ *
+ */
 public class Wind {
 	private Windart momentanerWind;
 
+	/**
+	 * Simuliert den Wind, sodass mit jeweils 20% Wahrscheinlichkeit nach links oder
+	 * rechts weht. Die Windart ist durch ein Enum auf drei Werte beschraenkt
+	 */
 	public void simuliereWind() {
 		double zufallsZahl = Math.random();
 		if (0 <= zufallsZahl && zufallsZahl <= 0.2) {
